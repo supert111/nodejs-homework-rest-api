e72e013f9c972df17b8a5f0f30e10716f339a184
supert111 <sportsmen@bigmir.net>
2021-07-03 10:33:04 +0300

    push

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'Home work' })
+  res.json({ message: 'Home work 2' })
 })
 
 router.get('/:contactId', async (req, res, next) => {
