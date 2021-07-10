26f65579a2ad269b67f9e007d37a5098a66e41af
supert111 <sportsmen@bigmir.net>
2021-07-11 02:31:41 +0300

    start

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  res.json({ message: 'hw03-mongodb' })
 })
 
 router.get('/:contactId', async (req, res, next) => {
